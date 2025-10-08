45196809fb1786cc787fca6c6bb03d443b9d8645
SrJoluDAM <josemonterc94@gmail.com>
2025-10-08 03:04:39 +0200

    update
    Primer programa intelliJ IDEA

---
+++ IntelliJ IDEA/Proyectoinicial/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main (String[] args){
+
+        System.out.println("Hola, este es mi primer programa en JAVA");
+        System.out.println("Este es mi segunda linea de codigo");
+        System.out.print("Linea de codigo 3");
+        System.out.println("Esta es mi cuartoa linea de codigo");
+    }
+}
